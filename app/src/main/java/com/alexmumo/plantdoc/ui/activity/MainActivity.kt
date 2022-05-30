23a88a1e9a928491bf6d2fe1b81c1e8eddd51b28
alexymumo <alexymumo2000@gmail.com>
2022-05-30 23:07:40 +0300

    project setupt

--- app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alexmumo.plantdoc
+package com.alexmumo.plantdoc.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alexmumo.plantdoc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
