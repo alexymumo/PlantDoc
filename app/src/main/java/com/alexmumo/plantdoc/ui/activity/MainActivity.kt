576eea45cb952b0d68ec552e4c2ba93398b10663
alexymumo <alexymumo2000@gmail.com>
2022-06-01 03:17:59 +0300

    navigation

--- app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
@@ -2,13 +2,33 @@ package com.alexmumo.plantdoc.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.alexmumo.plantdoc.R
 import com.alexmumo.plantdoc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.loginFragment,
+                R.id.registerFragment
+            ),
+            binding.drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
