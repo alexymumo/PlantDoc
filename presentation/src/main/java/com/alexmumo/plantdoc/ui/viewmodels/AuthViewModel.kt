95dd9161ee1681884d89b11e15a6f24570e6eac8
alexymumo <alexymumo2000@gmail.com>
2022-06-28 11:17:14 +0300

    navigation drawer

--- presentation/src/main/java/com/alexmumo/plantdoc/ui/viewmodels/AuthViewModel.kt
+++ presentation/src/main/java/com/alexmumo/plantdoc/ui/viewmodels/AuthViewModel.kt
@@ -2,7 +2,5 @@ package com.alexmumo.plantdoc.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.alexmumo.data.repository.AuthRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class AuthViewModel constructor(private val authRepository: AuthRepository) : ViewModel()
