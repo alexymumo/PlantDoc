44556b5ed8b8f797e1cd420bae9ad2aca4d48530
alexymumo <alexymumo2000@gmail.com>
2022-07-18 22:12:34 +0300

    bottom nav with fab

--- presentation/src/main/java/com/alexmumo/plantdoc/ui/fragments/auth/ForgotPassword.kt
+++ presentation/src/main/java/com/alexmumo/plantdoc/ui/fragments/auth/ForgotPassword.kt
@@ -1,21 +1,20 @@
-package com.alexmumo.plantdoc.ui.fragments.list
+package com.alexmumo.plantdoc.ui.fragments.auth
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.alexmumo.plantdoc.databinding.FragmentFarmersListBinding
+import com.alexmumo.plantdoc.databinding.FragmentForgotPasswordBinding
 
-
-class FarmersList : Fragment() {
-    private lateinit var binding: FragmentFarmersListBinding
+class ForgotPassword : Fragment() {
+    private lateinit var binding: FragmentForgotPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentFarmersListBinding.inflate(layoutInflater, container, false)
+        binding = FragmentForgotPasswordBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 }
