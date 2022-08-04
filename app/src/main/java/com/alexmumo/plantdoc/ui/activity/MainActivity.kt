7d6b231d7cff897ec0f13117a31e8eb46828c91d
alexymumo <alexymumo2000@gmail.com>
2022-08-04 16:34:53 +0300

    added bottom nav with fab

--- app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alexmumo/plantdoc/ui/activity/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.alexmumo.plantdoc.ui.activity
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.* // ktlint-disable no-wildcard-imports
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import com.alexmumo.plantdoc.R
 import com.alexmumo.plantdoc.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,33 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bottomNav.background = null
+        binding.bottomNav.menu.getItem(2).isEnabled = false
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navHostFragment.findNavController()
         NavigationUI.setupWithNavController(binding.bottomNav, navController)
-        // define high order destination
-        /*appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.profileFragment,
-                R.id.settingsFragment,
-            ),
-            binding.drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-
-        binding.navView.setupWithNavController(navController)
-
-         */
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.splashFragment || destination.id == R.id.registerFragment || destination.id == R.id.splashFragment) {
-                binding.bottomNav.visibility = View.GONE
-                // todo add toolbar visibility
-            } else {
-                binding.bottomNav.visibility = View.VISIBLE
-                // add toolbar visibility
-            }
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -53,4 +32,3 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
-
--- app/src/main/java/com/alexmumo/plantdoc/ui/fragments/splash/SplashViewModel.kt
+++ app/src/main/java/com/alexmumo/plantdoc/ui/fragments/splash/SplashViewModel.kt
@@ -1,6 +1,7 @@
 package com.alexmumo.plantdoc.ui.fragments.splash
 
 import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -8,8 +9,8 @@ import com.google.firebase.auth.FirebaseAuth
 class SplashViewModel : ViewModel() {
     var splash: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
     fun setUp() {
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             splash.value = FirebaseAuth.getInstance().currentUser == null
-        }, 3000)
+        }, 5000L)
     }
 }
