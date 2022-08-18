20a70f28b147c88342d0c0c8ae5d8e101bdfab49
alexymumo <alexymumo2000@gmail.com>
2022-08-18 23:28:33 +0300

    onboarding design

--- app/src/main/java/com/alexmumo/plantdoc/PlantApplication.kt
+++ app/src/main/java/com/alexmumo/plantdoc/PlantApplication.kt
@@ -6,4 +6,16 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class PlantApplication : Application()
 
+class User<T>(name: T) {
+    var user = name
+    init {
+        println(user)
+    }
+}
+
+fun main() {
+    val alex: User<String> = User<String>("alexmumo")
+    val mumo: User<Int> = User<Int>(1)
+}
+
 
